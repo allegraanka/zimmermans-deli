fe88077e9c4a5f9e77aa28bd22aecad3c2e64af0
Allegra Anka <allegra.anka@gmail.com>
2018-10-13 17:38:04 -0400

    ready to deploy

--- javascripts/scripts.js
+++
@@ -1,7 +0,0 @@
-// $(document).ready(function() {
-//    $('#linkToAbout').localScroll({duration:800});
-// });
-//
-// $(document).ready(function() {
-//    $('#linkToShop').localScroll({duration:800});
-// });
